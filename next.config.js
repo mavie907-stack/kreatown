3bdbeeecb477acab488eebba6f499a7ab288d0e8
İbrahim Toros <ibrtoros@icloud.com>
2026-03-08 22:24:49 +0300

    Fix next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-  output: undefined,
-}
-
+const nextConfig = {}
 module.exports = nextConfig
